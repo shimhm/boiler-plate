c1ab9a0b8d41b22690890ae8d8c34927e3b18db0
shimhm <55525917+shimhm@users.noreply.github.com>
2020-11-10 23:46:02 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        typde: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+//다른 파일에서 쓸 수 있도록 export
+module.exports={ User }
