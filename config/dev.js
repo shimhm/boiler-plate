997e59ce7b1b87003b7b3953a74a4b1c9eb4aeb8
shimhm <55525917+shimhm@users.noreply.github.com>
2020-11-11 09:41:41 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI: 'mongodb+srv://user:user1234@boiler-plate.tdzck.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://user:user1234@boiler-plate.tdzck.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!~~~')
+})
+
+app.post('/register',(req,res) => {
+  
+  //회원 가입할 때 필요한 정보들을 client에서 가져오면
+  //데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
